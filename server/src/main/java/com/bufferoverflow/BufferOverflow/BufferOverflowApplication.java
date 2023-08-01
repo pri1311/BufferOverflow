b523c91a0540f422ba15dc13d22a8b8a964b0055
Priya Nagda <nagdapriya13@gmail.com>
2023-08-01 22:47:08 +0530

    initial project setup

---
+++ server/src/main/java/com/bufferoverflow/BufferOverflow/BufferOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.bufferoverflow.BufferOverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BufferOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BufferOverflowApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/bufferoverflow/BufferOverflow/BufferOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bufferoverflow.BufferOverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BufferOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
